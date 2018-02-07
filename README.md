45b01f0d01e063cc2682797f27b4411e33d3ecec
Pieter Jordaan <pieter.jordaan@careem.com>
2018-02-07 13:10:36 +0100

    Updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # aws-cli-mfa
 MFA helper script for AWS cli 
+
+This script simplifies the process of getting STS tokens and adding the temporary credentials to credentials file. This is especially useful when you have mulitple AWS accounts and/or profiles that you need to manage temporary credentials for.
+
+The temporary STS credentials are stored under a new profile in you AWS credentials file, called mfa-<profile-name>. Whenever a modification is made to the credentials file, the existing credentials file is first backed-up. The serial number of the MFA device you use for each profile is cached locally so that you only need to provide it the first time that you use this tool.
+
+
+## Setup
+
+```bash
+$ git clone git@github.com:godfried/aws-cli-mfa.git
+$ cd aws-cli-mfa
+$ venv_dir=~/.venvs/aws-cli-mfa
+$ python3.6 -m venv ${env_dir}
+$ source ${env_dir}/bin/activate
+$ pip install -r requirements.txt
+$ python mfa_helper.py --help
+usage: mfa_helper.py [-h] -c CODE -p PROFILE [-r REGION] [-d DURATION]
+                     [-s SERIAL_NUMBER] [--cache-path CACHE_PATH]
+                     [--credentials-path CREDENTIALS_PATH]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CODE, --code CODE  Authentication code from mfa device.
+  -p PROFILE, --profile PROFILE
+                        AWS profile to get a session for.
+  -r REGION, --region REGION
+                        AWS region to use. (default: eu-west-1)
+  -d DURATION, --duration DURATION
+                        Duration in seconds to keep the STS token valid for.
+                        (default: 43200)
+  -s SERIAL_NUMBER, --serial-number SERIAL_NUMBER
+                        Serial number for your MFA device. ARN in the case of
+                        a virtual device. Falls back to a cached value in
+                        ~/.aws/serial_numbers.
+  --cache-path CACHE_PATH
+                        Path to the serial number cache. (default:
+                        ~/.aws/serial_numbers)
+  --credentials-path CREDENTIALS_PATH
+                        Path to the AWS credentials file. (default:
+                       ~/.aws/credentials)
+
+```
+
+## Usage
+
+Initial Usage
+```bash
+$ python mfa_helper.py --code=123456 --profile=aws-profile --serial-number=arn:aws:iam::123456789100:mfa/user
+2018-02-06 16:08:12,775 - aws-cli-mfa - INFO - Cached serial number 'arn:aws:iam::123456789100:mfa/user' for 'aws-profile' to '~/.aws/serial_numbers'
+2018-02-06 16:08:13,645 - aws-cli-mfa - INFO - Previous credentials backed up to '~/.aws/credentials.1517929693.637029'
+2018-02-06 16:08:13,646 - aws-cli-mfa - INFO - MFA credentials stored in profile 'mfa-aws-profile'
+```
+
+Using cached serial number:
+```bash
+$ python mfa_helper.py --code=654321 --profile=aws-profile
+2018-02-06 16:06:33,844 - aws-cli-mfa - INFO - Retrieved cached serial number 'arn:aws:iam::123456789100:mfa/user' for 'aws-profile'
+2018-02-06 16:06:35,089 - aws-cli-mfa - INFO - Previous credentials backed up to '~/.aws/credentials.1517929595.087957'
+2018-02-06 16:06:35,091 - aws-cli-mfa - INFO - MFA credentials stored in profile 'mfa-aws-profile'
+```
--- mfa_helper.py
+++ mfa_helper.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import logging
 import shutil
 from datetime import datetime
 from pathlib import Path
@@ -9,6 +10,23 @@ import boto3
 AWS_PATH = Path.home().joinpath(".aws")
 DEFAULT_SERIAL_NUMBER_CACHE = str(AWS_PATH.joinpath("serial_numbers"))
 DEFAULT_CREDENTIALS_PATH = str(AWS_PATH.joinpath("credentials"))
+
+
+def get_logger(level=logging.INFO):
+    log = logging.getLogger("aws-cli-mfa")
+    log.setLevel(level)
+    log.propagate = False
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    return log
+
+
+logger = get_logger()
 
 
 def get_cached_serial_number(cache_path, profile):
@@ -37,6 +55,8 @@ def cache_serial_number(cache_path, profile, serial_number):
     cache = read_cache(cache_path)
     cache[profile] = serial_number
     write_cache(cache_path, cache)
+    logger.info("Cached serial number '{}' for '{}' to '{}'"
+                .format(serial_number, profile, cache_path))
 
 
 def load_credentials(credentials_path):
@@ -52,20 +72,20 @@ def load_credentials(credentials_path):
 def save_credentials(credentials_path, creds):
     backup_path = "{}.{}".format(credentials_path, datetime.now().timestamp())
     shutil.copy(credentials_path, backup_path)
-    print("Previous credentials backed up to '{}'".format(backup_path))
+    logger.info("Previous credentials backed up to '{}'".format(backup_path))
     p = Path(credentials_path)
     with p.open(mode='w') as f:
         f.write("\n".join(creds))
 
 
-def credential_indices(creds, creds_profile):
-    profile_string = "[{}]".format(creds_profile)
-    if profile_string not in creds:
-        return len(creds), len(creds)
-    start_index = creds.index(profile_string)
-    end_index = len(creds)
-    for i in range(start_index + 1, len(creds)):
-        if creds[i].startswith("[") and creds[i].startswith("]"):
+def credential_indices(credentials, credentials_profile):
+    profile_string = "[{}]".format(credentials_profile)
+    if profile_string not in credentials:
+        return len(credentials), len(credentials)
+    start_index = credentials.index(profile_string)
+    end_index = len(credentials)
+    for i in range(start_index + 1, len(credentials)):
+        if credentials[i].startswith("[") and credentials[i].startswith("]"):
             end_index = i
             break
     return start_index, end_index
@@ -87,35 +107,37 @@ def update_mfa_credentials(credentials_path, mfa_profile_name, access_key,
     ]
     new_credentials = first + middle + last
     save_credentials(credentials_path, new_credentials)
-    print("MFA credentials stored in profile '{}'".format(mfa_profile_name))
+    logger.info(
+        "MFA credentials stored in profile '{}'".format(mfa_profile_name))
 
 
 def main():
-    p = argparse.ArgumentParser()
-    p.add_argument("-c", "--code", help="Authentication code from mfa device",
+    p = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument("-c", "--code", help="Authentication code from mfa device.",
                    required=True)
-    p.add_argument("-p", "--profile", help="AWS profile to get a session for",
+    p.add_argument("-p", "--profile", help="AWS profile to get a session for.",
                    required=True)
-    p.add_argument("-r", "--region", help="AWS region to use",
+    p.add_argument("-r", "--region", help="AWS region to use.",
                    default="eu-west-1")
     p.add_argument("-d", "--duration",
-                   help="Duration to keep the STS token valid for",
-                   default=43200,
-                   type=int)
+                   help="Duration in seconds to keep the STS token valid for.",
+                   default=43200, type=int)
     p.add_argument("-s", "--serial-number",
                    help="Serial number for your MFA device. "
-                        "ARN in the case of a virtual device. Falls back to "
-                        "cached value in {}"
+                        "ARN in the case of a virtual device. Falls back to a "
+                        "cached value in {}."
                    .format(DEFAULT_SERIAL_NUMBER_CACHE))
-    p.add_argument("--cache-path", help="Path to the serial number cache",
+    p.add_argument("--cache-path", help="Path to the serial number cache.",
                    default=DEFAULT_SERIAL_NUMBER_CACHE)
     p.add_argument("--credentials-path",
-                   help="Path to the AWS credentials file",
+                   help="Path to the AWS credentials file.",
                    default=DEFAULT_CREDENTIALS_PATH)
     args = p.parse_args()
-
     if args.serial_number is None:
         serial_number = get_cached_serial_number(args.cache_path, args.profile)
+        logger.info("Retrieved cached serial number '{}' for '{}'"
+                    .format(serial_number, args.profile))
         if serial_number is None:
             raise RuntimeError(
                 "No serial number provided for your mfa device and no serial "
